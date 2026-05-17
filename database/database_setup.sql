85a86d96f2f23e19b6778650a1eedf90c24aff91
HenrietteIraguha <h.iraguha@alustudent.com>
2026-05-18 00:05:56 +0200

    Create Table for Transactions

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -26,3 +26,41 @@ CREATE TABLE Users (
         COMMENT 'Brief explanation of what this category represents'
 
 );
+
+CREATE TABLE Transactions (
+
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT
+         COMMENT 'Internal unique identifier for each transaction',
+    
+    original_transaction_id VARCHAR(50) NULL
+         COMMENT 'original transaction ID from MOMO SMS',
+    
+    category_id INT NOT NULL
+        COMMENT 'Foreign Key linking to transaction category',
+    
+    amount DECIMAL(15,2) NOT NULL
+       COMMENT 'Transaction amount in Rwandan francs',
+    
+    fee DECIMAL(10,2) NULL
+        COMMENT 'fee charged for the transaction. 0 if no fee',
+    
+    balance_after DECIMAL(15,2) NULL
+        COMMENT 'Account balance after transaction was completed',
+    
+    transaction_date DATETIME NOT NULL
+        COMMENT 'Date and time when transaction occured',
+
+    status VARCHAR(10) NOT NULL
+        COMMENT 'Transaction status: Success or Failed',
+
+    raw_sms TEXT NOT NULL
+       COMMENT 'Complete original sms body',
+    
+    CONSTRAINT fk_category
+       FOREIGN KEY (category_id)
+       REFERENCES Transaction_categories(category_id),
+    
+    CONSTRAINT chck_status
+       CHECK(status IN ('Success','Failed'))
+
+);
