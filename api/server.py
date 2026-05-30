5c01d921415d438a72b3540ed0ad701c1740d1fb
HenrietteIraguha <h.iraguha@alustudent.com>
2026-05-30 15:19:19 +0200

    Add transaction data loader from  JSON file

--- api/server.py
+++ api/server.py
@@ -2,3 +2,13 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import base64
 import os
+
+json_path = os.path.join(os.path.dirname(__file__), '..', 'dsa', 'transactions.json')
+
+with open(json_path, 'r') as f:
+    transactions_list = json.load(f)
+
+transactions_dict = {t["transaction_id"]: t for t in transactions_list}
+
+VALID_USERNAME = "admin"
+VALID_PASSWORD = "MomoSms26"
