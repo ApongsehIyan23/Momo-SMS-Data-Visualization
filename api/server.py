c02448806cef2d15c222b9072e3c45b1286c69e3
HenrietteIraguha <h.iraguha@alustudent.com>
2026-05-30 15:22:31 +0200

    Add Basic Authentication credentials and Momo API class

--- api/server.py
+++ api/server.py
@@ -12,3 +12,26 @@ transactions_dict = {t["transaction_id"]: t for t in transactions_list}
 
 VALID_USERNAME = "admin"
 VALID_PASSWORD = "MomoSms26"
+
+class Momoapi(BaseHTTPRequestHandler):
+
+    def verify_user(self):
+        auth_header = self.headers.get('Authorization')
+
+        if not auth_header:
+            return False
+
+        encoded = auth_header.split(' ')[1]
+
+        decoded = base64.b64decode(encoded).decode('utf-8')
+
+        username, password = decoded.split(':')
+
+        return username == VALID_USERNAME and password == VALID_PASSWORD
+
+    def deny_access(self):
+        self.send_response(401)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        response = {"error": "Unauthorized. Invalid credentials."}
+        self.wfile.write(json.dumps(response).encode())
